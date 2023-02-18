b5f4440fc7d9a68282deeefe96c2deab263f5d30
Siddardha Annapureddy <94681169+SIDDARDHA5C0@users.noreply.github.com>
2023-02-18 17:06:14 +0000

    Added the html and css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 #importing the Flask
 app=Flask(__name__)
 #creating a flask object
 #decorator,registered an route
 @app.route("/")
 def helloworld():
-  return "hello ask the wrold"
+  return render_template("home.html")
 
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
