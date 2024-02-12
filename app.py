41cd2b3ddd6bcf2ba7899feeb0ee3efa419ef07c
SIDDARDHA5C0 <94681169+SIDDARDHA5C0@users.noreply.github.com>
2024-02-12 15:43:09 +0000

    used dynamic content and api route

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 #importing the Flask
-app=Flask(__name__)
+app = Flask(__name__)
 #creating a flask object
+
+JOBS = [
+  {
+    "id": 1,
+    "role": "data analyst",
+    "location": "india",
+    "salary": "100000"
+  },
+  {
+    "id": 2,
+    "role": "data scientist",
+    "location": "india",
+    "salary": "100000"
+  },
+  {
+    "id": 3,
+    "role": "data engineer",
+    "location": "india",
+    "salary": "100000"
+  },
+  {
+    "id": 4,
+    "role": "machine engineer",
+    "location": "india",
+    "salary": "100000"
+  },
+]
+
+
 #decorator,registered an route
 @app.route("/")
 def helloworld():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS,company_name="Jovian")
 
-if __name__=="__main__":
-  app.run(host="0.0.0.0",debug=True)
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
+
